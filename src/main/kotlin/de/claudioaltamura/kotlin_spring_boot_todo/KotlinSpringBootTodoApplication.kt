07d93f19c817cbff4079a33f738a8174b97dab70
claudioaltamura <claudio@claudioaltamura.de>
2024-09-16 11:24:05 +0200

    chore: added project setup

---
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/KotlinSpringBootTodoApplication.kt
@@ -0,0 +1,11 @@
+package de.claudioaltamura.kotlin_spring_boot_todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootTodoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootTodoApplication>(*args)
+}
---
+++ src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/KotlinSpringBootTodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.claudioaltamura.kotlin_spring_boot_todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootTodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

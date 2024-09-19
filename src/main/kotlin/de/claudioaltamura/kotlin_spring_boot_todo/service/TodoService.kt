55d32241844754905b932ec476ec2c3fffd49b38
claudioaltamura <claudio@claudioaltamura.de>
2024-09-19 12:40:55 +0200

    feat: value from application properties

--- src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/service/TodoService.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/service/TodoService.kt
@@ -1,13 +1,17 @@
 package de.claudioaltamura.kotlin_spring_boot_todo.service
 
 import de.claudioaltamura.kotlin_spring_boot_todo.controller.Todo
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class TodoService {
 
+    @Value("\${description}")
+    lateinit var description : String
+
     fun getTodo(id: Long): Todo {
-        return Todo(id, "first todo", "this is the first todo")
+        return Todo(id, "first todo", description)
     }
 
 }

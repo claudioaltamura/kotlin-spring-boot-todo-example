352dbf3593717c78223053db4f5969db029003ae
claudioaltamura <claudio@claudioaltamura.de>
2024-09-23 11:28:41 +0200

    refactor: moved to dtos

--- src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/dto/Todo.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/dto/Todo.kt
@@ -1,3 +1,3 @@
-package de.claudioaltamura.kotlin_spring_boot_todo.controller
+package de.claudioaltamura.kotlin_spring_boot_todo.dto
 
 data class Todo(val id: Long, val title: String, val description: String)

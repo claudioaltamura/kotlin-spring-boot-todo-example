10a767d8ca65cdcd6f72ff75f39f04c84054f0a3
claudioaltamura <claudio@claudioaltamura.de>
2024-09-27 13:31:06 +0200

    feat: added repository

---
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/TodoRepository.kt
@@ -0,0 +1,12 @@
+package de.claudioaltamura.kotlin_spring_boot_todo.repository
+
+import de.claudioaltamura.kotlin_spring_boot_todo.entity.TodoEntity
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+
+interface TodoRepository : CrudRepository<TodoEntity, Int> {
+
+    @Query(value = "SELECT * FROM TODOS WHERE title like %?1%", nativeQuery = true)
+    fun findByTodoTitle(title : String) : List<TodoEntity>
+
+}

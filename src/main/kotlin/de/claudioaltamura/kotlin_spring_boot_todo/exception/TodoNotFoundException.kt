dbb6939e977c0dcf260eece4142f4d78987e5d57
claudioaltamura <claudio@claudioaltamura.de>
2024-09-30 09:48:27 +0200

    feat: implemented get todo by id

---
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/exception/TodoNotFoundException.kt
@@ -0,0 +1,3 @@
+package de.claudioaltamura.kotlin_spring_boot_todo.exception
+
+class TodoNotFoundException(message: String)  : RuntimeException(message)
--- src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/service/TodoService.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/service/TodoService.kt
@@ -3,6 +3,7 @@ package de.claudioaltamura.kotlin_spring_boot_todo.service
 import de.claudioaltamura.kotlin_spring_boot_todo.dto.NewTodo
 import de.claudioaltamura.kotlin_spring_boot_todo.dto.Todo
 import de.claudioaltamura.kotlin_spring_boot_todo.entity.TodoEntity
+import de.claudioaltamura.kotlin_spring_boot_todo.exception.TodoNotFoundException
 import de.claudioaltamura.kotlin_spring_boot_todo.repository.TodoRepository
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
@@ -29,13 +30,21 @@ class TodoService(val todoRepository: TodoRepository) {
         } ?: todoRepository.findAll()
 
         return todos.map {
-            it.id?.let { it1 -> Todo(it1, it.title, it.description) }!!
+            Todo(it.id!!, it.title, it.description)
         }
     }
 
     fun getTodo(id: Int): Todo {
-        //TODO find by id
-        return Todo(id, "first todo", "description")
+        val existingTodo = todoRepository.findById(id)
+
+        return if (existingTodo.isPresent) {
+            existingTodo.get()
+                .let {
+                    Todo(it.id!!, it.title, it.description)
+                }
+        } else {
+            throw TodoNotFoundException("no todo found for the id '$id'")
+        }
     }
 
     fun updateTodo(id: Int, todo: Todo) {

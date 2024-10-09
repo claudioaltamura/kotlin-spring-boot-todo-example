1122a6bcf3f8f39c0e49e3d9ce4ed6cca3b1fddc
claudioaltamura <claudio@claudioaltamura.de>
2024-10-09 10:33:38 +0200

    test: added add test case

--- src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/controller/TodoControllerTest.kt
+++ src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/controller/TodoControllerTest.kt
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @WebMvcTest(TodoController::class)
@@ -24,6 +25,27 @@ class TodoControllerTest {
     lateinit var todoService: TodoService
 
     @Test
+    fun `should add a todo successfully`() {
+        val todo = Todo(1, "a todo", "this is a todo.")
+
+        every { todoService.addTodo(any()) }.returns(Todo(1, "a todo", "this is a todo."))
+
+        val createdToDo = webTestClient
+            .post()
+            .uri("/todos")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(todo)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(Todo::class.java)
+            .returnResult()
+            .responseBody
+
+        assertThat(createdToDo!!.id).isEqualTo(1)
+
+        verify { todoService.addTodo(any()) }
+    }
+        @Test
     fun `should return a todo when id given`() {
         every { todoService.getTodo(any()) }.returns(Todo(1, "a todo", "this is a todo."))
 

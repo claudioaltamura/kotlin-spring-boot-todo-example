3b294fabf683ab8873c344639b3e2d077e644920
claudioaltamura <claudio@claudioaltamura.de>
2024-11-07 09:44:40 +0100

    test: added data jpa test

---
+++ src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/CommentRepositoryTest.kt
@@ -0,0 +1,37 @@
+package de.claudioaltamura.kotlin_spring_boot_todo.repository
+
+import de.claudioaltamura.kotlin_spring_boot_todo.entity.CommentEntity
+import de.claudioaltamura.kotlin_spring_boot_todo.entity.TodoEntity
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+
+@DataJpaTest
+class CommentRepositoryTest @Autowired constructor(
+    val entityManager: TestEntityManager,
+    val commentRepository: CommentRepository,
+    val todoRepository: TodoRepository) {
+
+    @BeforeEach
+    fun setUp() {
+        commentRepository.deleteAll()
+        todoRepository.deleteAll()
+    }
+
+    @Test
+    fun findByTodo() {
+        val todo = TodoEntity(null,  "title", "this is a todo.")
+        entityManager.persist(todo)
+        val comment = CommentEntity(null,"comment", todo)
+        entityManager.persist(comment)
+        entityManager.flush()
+
+        val comments = commentRepository.findByTodo(todo.id!!)
+
+        assertThat(comments.size).isEqualTo(1)
+    }
+
+}

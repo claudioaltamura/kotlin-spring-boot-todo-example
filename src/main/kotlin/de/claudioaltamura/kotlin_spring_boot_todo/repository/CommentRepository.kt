d65c26586abd27826be6a55b623c2bd6a69e9b77
claudioaltamura <claudio@claudioaltamura.de>
2024-11-08 09:45:23 +0100

    refactor: switch to JPQL

--- src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/CommentRepository.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/CommentRepository.kt
@@ -3,10 +3,11 @@ package de.claudioaltamura.kotlin_spring_boot_todo.repository
 import de.claudioaltamura.kotlin_spring_boot_todo.entity.CommentEntity
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 interface CommentRepository : CrudRepository<CommentEntity, Long> {
 
-    @Query(value = "SELECT * FROM COMMENTS WHERE TODO_ID = ?1", nativeQuery = true)
-    fun findByTodo(todoId : Long) : List<CommentEntity>
+    @Query(value = "SELECT c FROM CommentEntity c WHERE c.todo.id = :todoId")
+    fun findByTodo(@Param("todoId") todoId : Long) : List<CommentEntity>
 
 }
--- src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/TodoRepository.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/TodoRepository.kt
@@ -3,10 +3,11 @@ package de.claudioaltamura.kotlin_spring_boot_todo.repository
 import de.claudioaltamura.kotlin_spring_boot_todo.entity.TodoEntity
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 interface TodoRepository : CrudRepository<TodoEntity, Long> {
 
-    @Query(value = "SELECT * FROM TODOS WHERE title like %?1%", nativeQuery = true)
-    fun findByTitle(title : String) : List<TodoEntity>
+    @Query(value = "SELECT t FROM TodoEntity t WHERE t.title like %:title%")
+    fun findByTitle(@Param("title") title : String) : List<TodoEntity>
 
 }

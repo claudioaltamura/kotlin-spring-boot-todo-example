62b8c38caaf8679c688fe19d6590ef42c4e416c6
claudioaltamura <claudio@claudioaltamura.de>
2024-11-19 22:25:30 +0100

    test: added initializer

---
+++ src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/PostgreSQLContainerInitializer.kt
@@ -0,0 +1,28 @@
+package de.claudioaltamura.kotlin_spring_boot_todo
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Testcontainers
+open class PostgreSQLContainerInitializer {
+
+    companion object {
+        @Container
+        val container = PostgreSQLContainer<Nothing>("postgres:latest").apply {
+            withDatabaseName("tododb")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", container::getJdbcUrl);
+            registry.add("spring.datasource.password", container::getPassword);
+            registry.add("spring.datasource.username", container::getUsername);
+        }
+    }
+
+}

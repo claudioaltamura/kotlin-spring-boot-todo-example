660a7c041b53040720efe2afe1c20da14f0c0547
claudioaltamura <claudio@claudioaltamura.de>
2024-11-22 14:37:04 +0100

    fix: build break

--- src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/TodoRepositoryTest.kt
+++ src/test/kotlin/de/claudioaltamura/kotlin_spring_boot_todo/repository/TodoRepositoryTest.kt
@@ -4,7 +4,6 @@ import de.claudioaltamura.kotlin_spring_boot_todo.AbstractDatabaseIntegrationTes
 import de.claudioaltamura.kotlin_spring_boot_todo.entity.TodoEntity
 import java.util.stream.Stream
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
